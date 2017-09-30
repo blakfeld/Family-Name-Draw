7e600b4139247e37b77907bab65acc9bb0930651
Corwin Brown <corwin@corwinbrown.com>
2017-09-30 13:05:38 -0500

    2017 version complete

--- schema.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS family_members;
-CREATE TABLE family_members(
-    id INTEGER Primary Key,
-    name TEXT,
-    chosen INTEGER,
-    has_chosen INTEGER,
-    chosen_by TEXT,
-    cannot_choose TEXT
-);
